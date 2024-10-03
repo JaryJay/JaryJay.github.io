a320428918f7a722767a7027408a9901fae34e64
Jay <jay.jrjren@gmail.com>
2024-10-03 12:29:40 -0400

    Update time management blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,8 +7,11 @@
 	<meta name="description" content="Welcome to Jay's blog." />
 </svelte:head>
 
-<LineDecoration class="absolute transform -rotate-12 origin-right" />
+<LineDecoration class="absolute origin-right transform -rotate-12" />
 <section class="home-section">
-	<h1 class="text-xl sm:text-2xl md:text-3xl text-stone-700 font-semibold">Blog</h1>
+	<h1 class="text-xl font-semibold sm:text-2xl md:text-3xl text-stone-700">Blog</h1>
 </section>
 <LineDecoration class="absolute transform rotate-12 -scale-x-100" />
+<section>
+	
+</section>
--- src/routes/blog/time-management/+page.svelte
+++ src/routes/blog/time-management/+page.svelte
@@ -1,21 +1,50 @@
-<section class="centered-content px-4">
+<section class="px-4 pb-8 leading-relaxed centered-content">
 	<div class="py-4">
 		<h2 class="text-xl font-semibold">How to actually get good at managing your time</h2>
 	</div>
 	<p>Disclaimer: Read all of my blog posts with a grain of salt.</p>
 	<p>
-		As someone who likes solving problems, I think time management is quite a unique and interesting
+		As someone who likes solving problems, I think time management is a unique and interesting
 		challenge. Here's some things I learned that helped me manage my time better.
 	</p>
-	<h3 class="text-lg font-medium">What do you want to spend your time on</h3>
+	<h3 class="text-lg font-medium">Define "good"</h3>
 	<p>
 		The first step to fixing bad time management is defining what good time management looks like.
+		In high school, I wanted to:
 	</p>
-	<h3 class="text-lg font-medium">Tracking your time</h3>
+	<ol class="indent-16">
+		<li>a&rpar; Spend the minimum amount of time on studying to get good grades (&ge;96)</li>
+		<li>b&rpar; Fulfill my responsibilities in school clubs (computer science club, math club)</li>
+		<li>c&rpar; Spend lots of time on extracurriculars</li>
+		<li>d&rpar; Spend lots of time on coding projects</li>
+		<li>e&rpar; Sleep &ge;8 hours a day</li>
+		<li>f&rpar; Still have some time to exercise, socialize, and have fun</li>
+	</ol>
+	<p>In university, my goals are similar but different.</p>
+	<ol class="indent-16">
+		<li>a&rpar; Spend the minimum amount of time on studying to get decent grades (&ge;80)</li>
+		<li>a&rpar; Spend lots of time applying to jobs, polishing my resume</li>
+		<li>d&rpar; Spend lots of time on coding projects</li>
+		<li>f&rpar; Regularly exercise, socialize, and have fun</li>
+		<li>e&rpar; Sleep &ge;8 hours a day</li>
+	</ol>
 	<p>
-		Let's say you do know to fixing bad time management is learning about where your time is
-		actually going.
+		I believe that listing down your goals immediately makes them more achievable. After all, you
+		can't succeed at anything if you don't have a clear definition of what success looks like!
 	</p>
+	<h3 class="text-lg font-medium">Analyzing the difference</h3>
+	<p>
+		So, you've decided what "good" time management looks like. Take the time to analyze the
+		difference between the quality (or lack thereof) of your current time management and your goal.
+	</p>
+	<p>
+		If you often feel like you don't even know where your time is going, (or simply don't want to
+		admit it), try tracking your screen time usage (because, after all, that's likely where all your
+		time is going).
+	</p>
+	<p>Spending too much time on Instagram and Reddit?</p>
+	<p>Spending too much time on gaming?</p>
+	<p>Spending too much time on... something else that isn't productive?</p>
 </section>
 
 <style>
