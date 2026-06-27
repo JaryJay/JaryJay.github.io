2a7f5ad4c125d7d5e290321b98bda96164d0ef19
Jay <jay.jrjren@gmail.com>
2026-06-26 23:43:41 -0700

    feat: new blog: "Conciseness"

--- src/lib/blogPosts.ts
+++ src/lib/blogPosts.ts
@@ -7,9 +7,17 @@ export interface BlogPost {
 
 export const blogPosts: BlogPost[] = [
 	{
+		title: 'Conciseness',
+		slug: 'conciseness',
+		date: '2026-06-26',
+		excerpt:
+			'Hitting the mandatory 1000-word minimum in essays was always difficult in high school.',
+	},
+	{
 		title: 'How to Actually Manage Your Time Better',
 		slug: 'time-management',
 		date: '2024-10-03',
-		excerpt: 'Analyzing the gap between your status quo and your goal, and putting systems in place to close it.',
+		excerpt:
+			'Analyzing the gap between your status quo and your goal, and putting systems in place to close it.',
 	},
 ];
---
+++ src/routes/blog/conciseness/+page.md
@@ -0,0 +1,11 @@
+# Conciseness
+
+2026-06-26
+
+---
+
+I've always struggled to write a lot of text; hitting the mandatory 1000-word minimum in essays was always difficult in high school.
+
+Previously, I felt that writing a short essay meant that I didn't have enough content. Now, in the age of genAI (and thus, an abundance of text), I've realized that conciseness is appreciated and often preferred.
+
+Write concisely.
